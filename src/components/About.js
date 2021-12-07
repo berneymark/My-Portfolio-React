4c0d38feebc34a0a93f88c9b79efad17195d7d95
Mark Berney <berneydwmark@gmail.com>
2021-12-07 14:03:09 +1300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,14 @@
-export default function About() {}
+export default function About() {
+    return(
+        <section id="about">
+            <div>
+                <div>
+                    <h1>
+                        Hi, I'm Mark.
+                        <br>I love to build amazing websites.</br>
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
+}
