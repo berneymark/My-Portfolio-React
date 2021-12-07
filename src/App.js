6b426cbe702235a7ca6e55c88efb835e52188bcb
Mark Berney <berneydwmark@gmail.com>
2021-12-07 16:53:38 +1300

    Added Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar/>
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Mark Berney</h1>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
