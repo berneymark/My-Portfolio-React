24eae15126071dda143aabe8839ffb7e590fcf50
Mark Berney <berneydwmark@gmail.com>
2021-12-07 20:02:54 +1300

    Added Contact.js + About.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import About from './components/About';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
