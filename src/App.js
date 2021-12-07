b00cf310dc01e3a8d4a3a49a35477fa60448d35e
Mark Berney <berneydwmark@gmail.com>
2021-12-07 20:03:08 +1300

    Setup Router pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import About from './components/About';
 import Contact from './components/Contact';
@@ -7,10 +7,33 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar/>
-      </header>
+    <Router>
+        <header>
+          <Navbar/>
+        </header>
+        <Routes>
+          <Route 
+            path="/" 
+            element={ <Home/> }
+          />
+          <Route 
+            path ="about" 
+            element={ <About/> }
+          />
+          <Route 
+            path="contact" 
+            element={ <Contact/> }
+          />
+        </Routes>
+    </Router>
+  );
+}
+
+const Home = (props) => {
+  return(
+    <div>
+      <h1>Home Page</h1>
+      <p>Lroem ipsum.</p>
     </div>
   );
 }
