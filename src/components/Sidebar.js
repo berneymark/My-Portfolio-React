96d5e234cef47ec6859df331c2fca033a12bd305
Mark Berney <berneydwmark@gmail.com>
2021-12-18 12:56:45 +1300

    UI tweaks & social accounts open in new tabs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,19 +5,28 @@ import TwitterLogo from '../assets/img/twitter-240.png';
 function Sidebar() {
     return (
         <div className="socials">
-            <a href="https://www.github.com/berneymark">
+            <a 
+                href="https://www.github.com/berneymark"
+                target="_blank"
+                rel="noopener noreferrer">
                 <img 
                     className="social-icons"
                     src={ GithubLogo }
                     alt="github-logo"/>
             </a>
-            <a href="https://www.linkedin.com/in/berneymark">
+            <a 
+                href="https://www.linkedin.com/in/berneymark"
+                target="_blank"
+                rel="noopener noreferrer">
                 <img 
                     className="social-icons"
                     src={ LinkedInLogo }
                     alt="linkedin-logo"/>
             </a>
-            <a href="https://www.twitter.com/berney_mark">
+            <a 
+                href="https://www.twitter.com/berney_mark"
+                target="_blank"
+                rel="noopener noreferrer">
                 <img
                     className="social-icons"
                     src={ TwitterLogo }
