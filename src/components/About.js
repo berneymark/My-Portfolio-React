aab6fd16b550e6c69553c08cca36f73650a861e5
Mark Berney <berneydwmark@gmail.com>
2021-12-21 09:31:41 +1300

    Added about me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ function About() {
     return (
         <div>
             <h1>About Me</h1>
+            <p>I am a software development graduate from Auckland University of Technology. I am seeking opportunities within the industry because my passion lies in delivering great products that users will enjoy. Also, I greatly enjoy the collaborative aspect of the role as I believe it's important for developers to be able to bounce off one another. This, along with my detailed oriented nature would make me a great fit for your team.</p>
         </div>
     );
 }
